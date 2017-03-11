ae2179656e370d4f4f2e859aea877d7f5f68c470
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2017-03-11 13:13:55 +0530

    removed unwanted and unnecessary files

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express(); // Web framework to handle routing requests
-var bodyParser = require('body-parser'),
+var bodyParser = require('body-parser');
 var PORT = process.env.PORT || 8080;
  app.use(bodyParser());
  app.use('/',express.static(__dirname));
