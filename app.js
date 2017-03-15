ebc27c3791aff6b059d36a81575ae81e31e757f2
Prahalad Belavadi <prahaladbelavadi@users.noreply.github.com>
2017-03-16 01:55:46 +0530

    change port from 8080 to 80

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express(); // Web framework to handle routing requests
 var bodyParser = require('body-parser');
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 80;
  app.use(bodyParser());
  app.use('/',express.static(__dirname));
  app.listen(PORT,function(){
